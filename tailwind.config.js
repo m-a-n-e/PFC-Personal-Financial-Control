b609135b7bc758c19e2bc57b5f74f8c54d2bd2b5
Mane <EmanuelDoernerS@gmail.com>
2024-04-23 00:49:21 -0300

    Finalizadas Páginas de Login e Cadastro

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      fontFamily: {
+        'archivo': ['Archivo Black', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif']
+      },
+      colors: {
+        'pfcBlack': {
+          100: '#131200',
+        },
+        'pfcFolly': {
+          100: '#ff1053',
+        },
+        'pfcWhite': {
+          100: '#fefdff',
+          200: '#f9f9f9',
+          300: '#f3f3f3',
+        },
+        'pfcViolet': {
+          100: '#52528C',
+        },
+        'pfcUltra': {
+          100: '#372554',
+        },
+      },
+    },
   },
   plugins: [],
 }
