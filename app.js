8e0b6a3de65d207762ee22541d13ecea76a349c5
Mane <EmanuelDoernerS@gmail.com>
2024-04-30 01:55:31 -0300

    Servi o diretório dist com o express

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Serve o diretório 'dist'
+app.use(express.static('dist'));
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'register.html'));
+});
+
+app.get(['/login', '/'], (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'login.html'));
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Aplicação rodando na porta ${port}`);
+});
+
+
+
+
+
+
+
+
